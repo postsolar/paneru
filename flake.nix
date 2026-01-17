304d29088acb51224e94dc2df4d4ba793fc7fd52
Alan <k5lqM94@gmail.com>
2026-01-17 22:21:42 +0200

    nix: Bump package version from 0.3.0 to 0.3.1 (#65)
    Ideally the version should be read from cargo, like in https://github.com/JakeStanger/ironbar/blob/f232bac6b04e53985d31b3d6061360d35ee62b26/flake.nix#L61-L67

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       pkgs = import nixpkgs { system = "aarch64-darwin"; };
       package = pkgs.rustPlatform.buildRustPackage {
         pname = "paneru";
-        version = "0.3.0";
+        version = "0.3.1";
         src = pkgs.lib.cleanSource ./.;
         postPatch = ''
           substituteInPlace build.rs --replace-fail \
